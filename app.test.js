6871c343fb9e10e4e4c9b0f978b2c047a8e8db79
masha <mashashepel2000@outlook.com>
2021-05-16 14:08:14 +0300

    Add separate tests for every start point

--- app.test.js
+++ app.test.js
@@ -1,58 +1,72 @@
 const app = require('./app')
 
 function eqSet(setA, setB) {
-    if (setA.size !== setB.size) return false;
-    for (let a of setA) if (!setB.has(a)) return false;
-    return true;
+    if (setA.size !== setB.size) return false
+    for (let a of setA) if (!setB.has(a)) return false
+    return true
 }
 
-test('Finding distinct reachable graph points', () => {
-    expect(
-        eqSet(
-            app.findReachable(app.graph, 1),
-            new Set([1, 2, 3, 4])
-        )
-    ).toBe(true);
+describe('Finding reachable points of graph', () => {
+    it('should return [1,2,3,4] when starting on 1', () => {
+        expect(
+            eqSet(
+                app.findReachable(app.graph, 1),
+                new Set([1, 2, 3, 4])
+            )
+        ).toBe(true)
+    })
 
-    expect(
-        eqSet(
-            app.findReachable(app.graph, 2),
-            new Set([1, 2, 3, 4])
-        )
-    ).toBe(true);
+    it('should return [1,2,3,4] when starting on 2', () => {
+        expect(
+            eqSet(
+                app.findReachable(app.graph, 2),
+                new Set([1, 2, 3, 4])
+            )
+        ).toBe(true)
+    })
 
-    expect(
-        eqSet(
-            app.findReachable(app.graph, 3),
-            new Set([1, 2, 3, 4])
-        )
-    ).toBe(true);
+    it('should return [1,2,3,4] when starting on 3', () => {
+        expect(
+            eqSet(
+                app.findReachable(app.graph, 3),
+                new Set([1, 2, 3, 4])
+            )
+        ).toBe(true)
+    })
 
-    expect(
-        eqSet(
-            app.findReachable(app.graph, 4),
-            new Set([1, 2, 3, 4])
-        )
-    ).toBe(true);
+    it('should return [1,2,3,4] when starting on 4', () => {
+        expect(
+            eqSet(
+                app.findReachable(app.graph, 4),
+                new Set([1, 2, 3, 4])
+            )
+        ).toBe(true)
+    })
 
-    expect(
-        eqSet(
-            app.findReachable(app.graph, 5),
-            new Set([])
-        )
-    ).toBe(true);
+    it('should return empty list when starting on 5', () => {
+        expect(
+            eqSet(
+                app.findReachable(app.graph, 5),
+                new Set([])
+            )
+        ).toBe(true)
+    })
 
-    expect(
-        eqSet(
-            app.findReachable(app.graph, 6),
-            new Set([1, 2, 3, 4, 5, 7])
-        )
-    ).toBe(true);
+    it('should return [1,2,3,4,5,7] when starting on 6', () => {
+        expect(
+            eqSet(
+                app.findReachable(app.graph, 6),
+                new Set([1, 2, 3, 4, 5, 7])
+            )
+        ).toBe(true)
+    })
 
-    expect(
-        eqSet(
-            app.findReachable(app.graph, 7),
-            new Set([1, 2, 3, 4])
-        )
-    ).toBe(true);
+    it('should return [1,2,3,4] when starting on 7', () => {
+        expect(
+            eqSet(
+                app.findReachable(app.graph, 7),
+                new Set([1, 2, 3, 4])
+            )
+        ).toBe(true)
+    })
 })
